2159a12d8105bb5329232b681ae416db486e850a
UshuaiaLang <50253332+UshuaiaLang@users.noreply.github.com>
2024-08-13 22:43:09 +0200

    08-13

---
+++ nodejs/standard_module/middleware/get.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const https = require('https');
+
+http.createServer((req,res)=>{
+    const myURL = new URL(req.url,'http://localhost:3000');
+    res.setHeader('content-type','application/json');
+    res.setHeader('Access-Control-Allow-Origin','*');
+
+    switch(myURL.pathname){
+        case '/api/user':
+            httpGet(res);
+            break;
+        default:
+            res.end('404');
+    }
+}).listen(3000);    
+
+function httpGet(response){
+    const url = 'https://i.maoyan.com/api/mmdb/movie/v3/list/hot.json?ct=%E5%8C%97%E4%BA%AC&ci=1&channelId=4'
+    var data = '';
+    https.get(url,(res)=>{
+
+       res.on('data',(chunk)=>{
+         data += chunk;
+       })   
+
+       res.on('end',()=>{
+        console.log(data);
+        response.end(data);
+
+       })
+
+    })
+}
