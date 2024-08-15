d94874397f98ea04239384ca4f5c9734af927cb0
UshuaiaLang <50253332+UshuaiaLang@users.noreply.github.com>
2024-08-15 15:57:27 +0200

    08-15

--- nodejs/express/index.js
+++ nodejs/express/index.js
@@ -2,19 +2,25 @@ const express = require("express")
 
 const app = express()
 
-app.get("/",(req,res)=>{
-    //res.send("hello world")
-    res.send({
-        name:"lang",
-        age:26
-    })
-})
+func1 = (req,res,next)=>{
+    console.log("token通过")
+    const isValid = true
+    if(isValid){
+        next()
+    }else{
+        res.send("error")
+    }
+}
+
+func2 = (req,res)=>{
+    res.send({list:[1,2,3]})
+}
+
+app.get("/home",[func1,func2])
 
 app.get("/login",(req,res)=>{
-    res.write("login")
-    res.end()
+    res.send("login")
 })
-
 
 app.listen(3000,()=>{
     console.log("server start");
