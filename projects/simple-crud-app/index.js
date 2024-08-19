49437c1a1086f8b1486252e8a1bfb68958a88853
UshuaiaLang <50253332+UshuaiaLang@users.noreply.github.com>
2024-08-19 22:13:06 +0200

    08-19

--- projects/simple-crud-app/index.js
+++ projects/simple-crud-app/index.js
@@ -4,6 +4,7 @@ const Product = require("./model/product_model")
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 app.get('/', (req, res) => {
     res.send('hello api');
@@ -15,30 +16,63 @@ app.get('/api/products', async (req, res) => {
         const products = await Product.find({})
         res.status(200).json(products)
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
 //get single product
-app.get('/api/products/:id', async (req, res) => {
+app.get('/api/product/:id', async (req, res) => {
     try {
-        const {id} = req.params
+        const { id } = req.params
         const product_id = await Product.findById(id)
         res.status(200).json(product_id)
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
+//commit products
 app.post('/api/products', async (req, res) => {
     try {
         const product = await Product.create(req.body)
         res.status(200).json(product)
     } catch (error) {
-        res.status(500).json({message: error.message})
+        res.status(500).json({ message: error.message })
     }
 })
 
+//update a product
+app.put('/api/product/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const product = await Product.findByIdAndUpdate(id, req.body)
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" })
+        } else {
+            const updatedProduct = await Product.findById(id)
+            res.status(200).json(updatedProduct)
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+//delete a product
+app.delete('/api/product/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const product = await Product.findByIdAndDelete(id)
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" })
+        } else {
+            res.status(200).json({ message: "Product deleted successfully" })
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+//connet mongodb
 mongoose.connect('mongodb://localhost:27017/demo')
     .then(() => {
         console.log("Connected to database!");
@@ -50,3 +84,4 @@ mongoose.connect('mongodb://localhost:27017/demo')
         console.log("Connected failed");
     })
 
+    
