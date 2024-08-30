c8542a59243cabcaa14d94b26716ddc3fbda7e53
UshuaiaLang <50253332+UshuaiaLang@users.noreply.github.com>
2024-08-30 11:53:40 +0200

    08-30

--- kerwinReact/reactappbylang/src/04-router/App.js
+++ kerwinReact/reactappbylang/src/04-router/App.js
@@ -1,17 +1,10 @@
 import React from 'react'
-import {HashRouter,Route,Link,Switch} from 'react-router-dom'
-import Films from './views/Films'
-import Cinemas from './views/Cinemas'
-import Center from './views/Center'
+import MRouter from './router/IndexRouter'
 
 export default function App() {
   return (
     <div>
-      <HashRouter>
-        <Route path="/films" component={Films}></Route>
-        <Route path="/cinemas" component={Cinemas}></Route>
-        <Route path="/center" component={Center}></Route>
-      </HashRouter>
+      <MRouter></MRouter>
     </div>
   )
 }
---
+++ kerwinReact/reactappbylang/src/04-router/router/IndexRouter.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { HashRouter, Route, Redirect, Switch } from 'react-router-dom'
+import Films from '../views/Films'
+import Cinemas from '../views/Cinemas'
+import Center from '../views/Center'
+import NotFound from '../views/NotFound'
+
+export default function MRouter() {
+    return (
+        <div>
+            <HashRouter>
+                <Switch>
+                    <Route path="/films" component={Films}></Route>
+                    <Route path="/cinemas" component={Cinemas}></Route>
+                    <Route path="/center" component={Center}></Route>
+                    <Redirect from="/" to="/films" exact></Redirect>
+                    <Route component={NotFound}></Route>
+                </Switch>
+            </HashRouter>
+        </div>
+    )
+}
