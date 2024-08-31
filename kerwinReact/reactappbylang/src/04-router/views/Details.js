45dc58d14b30b7ac202c5879b80b3d96ee2c13a9
UshuaiaLang <50253332+UshuaiaLang@users.noreply.github.com>
2024-08-31 22:46:24 +0200

    08-31

--- kerwinReact/reactappbylang/src/04-router/views/Details.js
+++ kerwinReact/reactappbylang/src/04-router/views/Details.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Details(props) {
-  console.log(props.match.params.filmId)
+    const [films, setFilms] = useState([])
+    let displayId = parseInt(props.match.params.filmId)
 
-  return (
-    <div>
-      Details
-    </div>
-  )
+    useEffect(() => {
+        axios.get('https://m.maizuo.com/gateway?cityId=110100&pageNum=1&pageSize=10&type=1&k=5588185', {
+            headers: {
+                'X-Client-Info': '{"a":"3000","ch":"1002","v":"5.2.1","e":"169701165652317177385000","bc":"110100"}',
+                'X-Host': 'mall.film-ticket.film.list'
+            }
+        }).then(res => {
+            setFilms(res.data.data.films)
+        })
+    }, [])
+
+    return (
+        <div>
+            <ul>
+                {
+                    films.filter(item => item.filmId === displayId).map(item => (
+                        <li key={item.filmId}>
+                            <div>{item.name}</div>
+                            <div style={{ width: '600px' }}>{item.synopsis}</div>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
 }
