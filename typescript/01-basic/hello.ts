6e6ca23a26f94f12ae7ae2a1a720ff81a7745758
UshuaiaLang <50253332+UshuaiaLang@users.noreply.github.com>
2024-09-02 20:23:42 +0200

    09-02

---
+++ typescript/01-basic/hello.ts
@@ -0,0 +1,9 @@
+// 你好世界
+//console.log("Hello World");
+
+function greet(person: string, date: Date) {
+    console.log(`Hello ${person}, today is ${date}!`);
+  }
+  
+greet("Brendan", new Date());
+
