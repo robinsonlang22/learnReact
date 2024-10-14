78af84e26cb530059e524b4d8d08807f8630e42a
UshuaiaLang <50253332+UshuaiaLang@users.noreply.github.com>
2024-10-14 22:47:56 +0200

    10-14

---
+++ 01-javascript/coderwhy/12_thread_queue/thread.js
@@ -0,0 +1,34 @@
+// js是单线程的，但是有同步任务和异步任务
+// 同步任务在主线程上执行，异步任务通过事件循环机制来执行
+// 异步任务又分为宏任务和微任务，微任务执行时机优于宏任务
+
+// 事件循环： js thread => 其他线程(耗时操作) => 回调函数到事件队列 => js thread 执行函数
+// macro task: setTimeout, ajax, DOM, UI rendering
+// micro task: queueMicrotask, Promise, async/await
+
+// main script
+console.log("script start");
+
+// macro task
+setTimeout(() => {
+    console.log("setTimeout");
+}, 1000);
+
+// micro task
+queueMicrotask(() => {
+    console.log("queueMicrotask");
+});
+
+// main script
+const nihao = () => {
+    console.log("nihao");
+};
+nihao();
+
+// micro task
+Promise.resolve().then(() => {
+    console.log("Promise");
+});
+
+// main script
+console.log("script end");
