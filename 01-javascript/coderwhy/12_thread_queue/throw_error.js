751a9eb196cff9c7f41167b2deefd408d9de3f7c
UshuaiaLang <50253332+UshuaiaLang@users.noreply.github.com>
2024-10-16 15:08:02 +0200

    10-15

--- 01-javascript/coderwhy/12_thread_queue/throw_error.js
+++ 01-javascript/coderwhy/12_thread_queue/throw_error.js
@@ -1,7 +1,8 @@
 function sum(a, b) {
     try {
         if (typeof a !== 'number' || typeof b !== 'number') {
-            throw "error";
+            //throw new Error('type should be number');
+            throw new TypeError('type should be number');
         }
         return a + b;
     } catch (error) {
@@ -9,4 +10,4 @@ function sum(a, b) {
     }
 }
 
-console.log(sum(1, 1));
+console.log(sum(false, 1));
